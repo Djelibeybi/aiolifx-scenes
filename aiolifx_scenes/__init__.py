32e7a28ea67cb766a076826295d0cb0a650fe5ac
Avi Miller <me@dje.li>
2023-03-17 19:13:46 +1100

    Bump version: 0.0.0 → 0.1.0

--- aiolifx_scenes/__init__.py
+++ aiolifx_scenes/__init__.py
@@ -4,6 +4,6 @@ from .main import async_get_scenes, get_scenes
 
 __author__ = """Avi Miller"""
 __email__ = 'me@dje.li'
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 
 __all__ = ["get_scenes", "async_get_scenes"]
